d60862068893a6bd3120e4dc89ad020706b2acca
MDSONU22943 <mdsonu22943@gmail.com>
2023-10-11 22:58:57 +0530

    o5passwordgenarator

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import './App.css'
+
+import Card from './components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1 className='text-3xl bg-green-500 p-3 rounded-md'>Vite with Tailwind</h1>
+     <Card username="hitesh/>
+     <Card/>
+     <Card/>
+    </>
+  )
+}
+
+export default App
